21004f5b2d4f32dd99885ed28c07d3beb1dc15dd
Nabin Bhandari <46418794+Nabiin@users.noreply.github.com>
2020-09-10 17:49:25 +0545

    create react app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Person from './Person/Person'
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+ Hello my name is  nabin
+      </header>
+      <Person />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const person = ()=>{
+    return <p>Hello World this is my first function component</p>
+}
+
+export default person
