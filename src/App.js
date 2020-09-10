87df0fa08231d6c9117dc49c92f9888e213ee827
Nabin Bhandari <46418794+Nabiin@users.noreply.github.com>
2020-09-10 17:54:41 +0545

    understanding class component and function component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
-import React from "react";
+import React , {Component} from "react";
 import "./App.css";
 import Person from './Person/Person'
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
- Hello my name is  nabin
-      </header>
-      <Person />
-    </div>
-  );
+
+//funciton component
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//          Hello my name is  nabin
+//       </header>
+//       <Person />
+//     </div>
+//   );
+// }
+
+
+
+//Class component
+class App extends Component{
+  render() {
+    return (
+          <div className="App">
+            <header className="App-header">
+               Hello my name is  nabin this is class Component
+            </header>
+            <Person />
+          </div>
+        );
+  }
 }
+
+
+
 
 export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,10 @@
 import React from 'react'
 
+
+//class component
 const person = ()=>{
     return <p>Hello World this is my first function component</p>
 }
+
 
 export default person
